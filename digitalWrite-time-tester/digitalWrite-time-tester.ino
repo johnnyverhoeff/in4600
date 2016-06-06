fb89d725ff885a2ca8fc4e37212a6539e06e9494
Johnny Verhoeff <j.verhoeff@me.com>
2016-06-06 12:17:21 +0200

    Adds project to determine the times of certain functions

---
+++ digitalWrite-time-tester/digitalWrite-time-tester.ino
@@ -0,0 +1,16 @@
+void setup() {
+  Serial.begin(115200);
+}
+
+uint32_t t = 0;
+
+void loop() {
+  for (uint32_t i = 0; i < 10000; i++) {
+    uint32_t it = micros();
+    digitalWrite(1, 0);
+    t += (micros() - it);
+  }
+
+  Serial.println((t / 10000));
+  t = 0;
+}
