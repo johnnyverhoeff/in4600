8ce1eae498caf21845dc0d14020d1dd99e6cbc2c
Johnny Verhoeff <j.verhoeff@me.com>
2016-07-03 17:20:02 +0200

    Adds blinking test w/ Manchester enc. -> no flikkering
    No flickering but the m-seq codes dont work like that

---
+++ led-blink-tests/adv-blink-delay-all-scenarios-random-manchester/adv-blink-delay-all-scenarios-random-manchester.ino
@@ -0,0 +1,45 @@
+#define led 13
+
+#define NUM_TIMES_MODULATE 4
+
+
+void setup() {
+  Serial.begin(250000);
+
+  pinMode(led, OUTPUT);
+  digitalWrite(led, LOW);
+
+  randomSeed(analogRead(A15));
+
+
+}
+
+void loop() {
+
+
+  digitalWrite(led, 1);
+  delay(5);
+  
+  int i = (int)random(0, (1 << NUM_TIMES_MODULATE));
+  
+  if ((i < 0) || (i > 15)) {
+    Serial.println("!!!!");
+  }
+  
+  for (int k = 0; k < NUM_TIMES_MODULATE; k++) {
+    int new_state = (i & (1 << k)) > 0;
+
+    digitalWrite(led, new_state);
+    delayMicroseconds(500);
+
+    digitalWrite(led, !new_state);
+    delayMicroseconds(500);
+    
+  }
+  
+  digitalWrite(led, 1);
+  delayMicroseconds(1000);
+        
+  
+
+}
